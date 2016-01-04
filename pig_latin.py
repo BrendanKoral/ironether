314e37c431a484595d5072eae74098759e5d3e43
Brendan Koral <brendanhkoral@gmail.com>
2016-01-04 13:23:54 -0600

    Add comments to code

--- pig_latin.py
+++ pig_latin.py
@@ -1,10 +1,16 @@
+#Create ending for pig latin words as a variable
 pyg = 'ay'
 
+#Take user input of word
 original = input('Enter a word:')
 
+#Check if user input is empty, only alphabetical characters
 if len(original) > 0 and original.isalpha():
-    word = original.lower()
-    first = word[0]
+    #Return original variable as lowercase
+	word = original.lower()
+    #Hold first letter of user input
+	first = word[0]
+	#Take user input, minus first letter, plus other variables and print it
     new_word = word[1:len(word)] + first + pyg
     print(new_word)
 else:
