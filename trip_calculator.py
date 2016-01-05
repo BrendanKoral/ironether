341294fbcc2ab10086600084fd23dde3d00c3307
Brendan Koral <brendanhkoral@gmail.com>
2016-01-04 20:41:39 -0600

    Add user input to Trip Calculator instead of having values that had to be entered within the script body

--- trip_calculator.py
+++ trip_calculator.py
@@ -1,3 +1,9 @@
+city = input("Choose a city: Charlotte, Tampa, Pittsburgh, or Los Angeles: ")
+
+days = int(input("Enter number of days for the trip: "))
+
+spending_money = int(input("Enter your desired amount of spending money: "))
+
 def hotel_cost(nights):
     return 140 * nights
     
@@ -21,4 +27,4 @@ def rental_car_cost(days):
     
 def trip_cost(city, days, spending_money):
     return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
-print trip_cost("Los Angeles", 5, 600)
+print("The total cost of your trip is: $" + str(trip_cost(city, days, spending_money)))
